0a36f87c9dd5a29bf6e3ce38b6116cba8292f4e9
datorima <davi.ribeiro@dcomp.ufs.br>
2025-07-17 16:36:13 -0300

    Update menage.js

--- menage.js
+++ menage.js
@@ -1,5 +1,5 @@
 
 const lista = [100,80,60,120,90]
-const soma = lista.reduce((acc,numero)=>acc + numero,1)
+const soma = lista.reduce((acc,numero)=>acc + numero,0)
 const novaLista = lista.map((numero)=> soma)
 console.log(novaLista)
