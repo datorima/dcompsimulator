05710bd0d1bec9afa707f2bb47895ebe2436ee1c
datorima <davi.ribeiro@dcomp.ufs.br>
2025-08-16 12:29:26 -0300

    Update logicaDoJogo.js

--- logicaDoJogo.js
+++ logicaDoJogo.js
@@ -1,3 +1,34 @@
+const freshPlayer = [
+    {idDaCena: 'inicio',
+    mochila: [],
+    genero: null}
+]
+const personagens = [
+    {nome: 'Good Ways', 
+        avatar:{
+            feliz: 'dcompsimulator/assets/goodways.png',
+            triste: 'dcompsimulator/assets/sadways.png',
+            bravo: 'dcompsimulator/assets/badways.png'
+        } 
+    },
+    {nome: 'Kal-El Freira', 
+        avatar:{
+            feliz: 'dcompsimulator/assets/kal-elfeliz.png',
+            triste: 'dcompsimulator/assets/kal-eltriste.png',
+            bravo: 'dcompsimulator/assets/kal-elbravo.png'
+        } 
+    },
+    {nome: 'Big Carlos', 
+        avatar:{
+            feliz: 'dcompsimulator/assets/bigcarlos-feliz.png',
+            triste: 'dcompsimulator/assets/bigcarlos-triste.png',
+            bravo: 'dcompsimulator/assets/bigcarlos-bravo.png'
+        } 
+    },
+    {nome: 'Pinguitor',
+     avatar: 'dcompsimulator/assets/pinguitor.png'
+    }
+]
 const cenas = [
   {
     id: 'inicio',
@@ -5,7 +36,7 @@ const cenas = [
     personagem: 'Pinguitor',
     dialogo: 'Olá, seja bem-vindo ao DCOMP! Eu sou o Pinguitor, arauto da atlética Bugados e serei seu guia',
     opcoes: [
-      a: 'Vamos lá!' , efeito: null, proximaCenaId: 'escolhadegenero'
+      {a: 'Vamos lá!' , efeito: null, proximaCenaId: 'escolhadegenero'}
     ]
   },
   {
@@ -14,8 +45,8 @@ const cenas = [
     personagem: 'Pinguitor',
     dialogo: 'Antes, vamos escolher o gênero do seu avatar:',
     opcoes: [
-      a: 'Sou homem!' , efeito: null, genero: 'masculino', proximaCenaId: 'explicacaodasrotas1',
-      b: 'Sou mulher!' , efeito: null, genero: 'feminino', proximaCenaId: 'explicacaodasrotas1'
+      {a: 'Sou homem!' , efeito: null, genero: 'masculino', proximaCenaId: 'explicacaodasrotas1'},
+      {b: 'Sou mulher!' , efeito: null, genero: 'feminino', proximaCenaId: 'explicacaodasrotas1'}
     ]
   },
   {
@@ -24,7 +55,7 @@ const cenas = [
     personagem: 'Pinguitor',
     dialogo: 'Ok então, vamos lá! Primeiro, você precisa saber sobre um detalhe: você poderá escolher rotas! Teremos 3 rotas e você deve escolher uma delas. Fazendo tudo na ordem certa, você chegerá ao final e terá uma grande surpresa!',
     opcoes: [
-      a: '(Próxima cena)' , efeito: null, proximaCenaId: 'explicacaodasrotas2'
+      {a: '(Próxima cena)' , efeito: null, proximaCenaId: 'explicacaodasrotas2'}
     ]
   },
   {
@@ -33,21 +64,26 @@ const cenas = [
     personagem: 'Pinguitor',
     dialogo: 'Mas, nem tudo são flores! Você vai ter que agradar os personagens se não vai perder pontos. Agora irei te explicar como funciona o jubilômetro',
     opcoes: [
-      a: '(Próxima cena)' , efeito: null, proximaCenaId: 'explicacaodojubilometro1'
-  },
+      {a: '(Próxima cena)' , efeito: null, proximaCenaId: 'explicacaodojubilometro1'}
+      ]
+    },
   {
     id: 'explicacaodojubilometro1',
     background: 'dcompsimulator/assets/dcompbg.jpg',
     personagem: 'Pinguitor',
     dialogo: 'O jubilômetro é o seu porto-seguro: ele começa em 100% e garante sua permanência na universidade. Cada resposta dada aos personagens que você verá em breve gera uma reação, então é melhor observar bem os detalhes, pois você perderá pontos se responde errado',
     opcoes: [
-      a: '(Próxima cena)' , efeito: null, proximaCenaId: 'explicacaodojubilometro2'
+      {a: '(Próxima cena)' , efeito: null, proximaCenaId: 'explicacaodojubilometro2'}
     ]
   },
   {
     id: 'explicacaodojubilometro2',
     background: 'dcompsimulator/assets/dcompbg.jpg',
     personagem: 'Pinguitor',
-    dialogo: 'Se seu contador chegar a 0% você é JUBILADO! Você volta pro começo e tem que refazer a rota que escolheu ou tentar a sorte em outra',
+    dialogo: 'Se seu contador chegar a 0% você é JUBILADO! Você volta pro começo e tem que refazer a rota que escolheu ou tentar a sorte em outra. Conseguiu entender?',
+    opcoes: [
+      {a: 'Sim!' , efeito: null, proximaCenaId: 'selecaodasrotas'},
+      {b: 'Não!' , efeito: null, proximaCenaId: 'explicacaodojubilometro1'}
+    ]
   }
 ]
